8ee884d5236fa0a983836548f14ea5affeeba0eb
Evan Ordona <eordona@umd.edu>
2024-08-15 01:19:30 -0400

    changed server

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 // https://mak-server.onrender.com
 // http://localhost:5000
-const URL = 'https://mak-server.onrender.com';
+//'https://lordsduel-api.fly.dev'
+const URL = 'https://lordsduel-api.fly.dev';
 const socket = io(URL)
 export default socket
